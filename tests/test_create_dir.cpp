fa6605dc9957a1e099e0fde0e181eb0a4e00ec28
gabime <gmelman1@gmail.com>
2024-04-29 19:52:34 +0300

    Fix compile

--- tests/test_create_dir.cpp
+++ tests/test_create_dir.cpp
@@ -85,7 +85,7 @@ TEST_CASE("dir_name", "[create_dir]") {
 #ifdef _WIN32
 
 //
-// test windows cases when drive letter is given e.g. C:\\some-folder\
+// test windows cases when drive letter is given e.g. C:\\some-folder
 //
 #include <windows.h>
 #include <fileapi.h>
